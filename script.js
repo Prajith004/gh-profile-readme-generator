c973543d51d4e37aac3aea084ec7961c76de2e4c
Prajith J Shetty <prajithjshetty1@gmail.com>
2023-10-07 20:51:56 +0530

    Github Profile Generator
    GitHub Profile READMEs. A GitHub Profile README is a special markdown file that allows users to showcase their skills, projects, and personality directly on their GitHub profile.

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayHTML() {
+  // Get user input
+  var hi = document.getElementById("hi").value;
+  var name = document.getElementById("name").value;
+  var subtitle=document.getElementById("subtitle").value;
+
+  // Create a template for the resume
+  var resumeTemplate = `
+  <h1 align="center">${hi},${name}</h1>
+  <h3 align="center">${subtitle}</h3>
+  `;
+
+  // Display the generated resume
+  document.getElementById("displaycode").innerHTML = resumeTemplate;
+}
