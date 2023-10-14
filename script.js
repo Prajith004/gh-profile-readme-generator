5a9c9a3509523f5f2b3972e2e84826c67e9161fa
Prajith J Shetty <prajithjshetty1@gmail.com>
2023-10-14 18:12:51 +0530

    links of social changed

--- script.js
+++ script.js
@@ -70,9 +70,9 @@ var linkedin=document.getElementById("linkedin").value;
 -${funfact} **${funfactPlaceholder}**
 <h3 align="left">Connect with me:</h3>
 <p align="left">
-<a href="https://linkedin.com/in/${linkedin}" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"  height="30" width="40" /></a>
-<a href="https://instagram.com/${instagram}" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg"  height="30" width="40" /></a>
-<a href="https://www.youtube.com/c/${youtube}" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/youtube.svg"  height="30" width="40" /></a>
+<a href="https://linkedin.com/in/${linkedin}" target="blank"><img align="center" src="https://github.com/Prajith004/gh-profile-readme-generator/blob/main/images/linkedin.svg"  height="30" width="40" /></a>
+<a href="https://instagram.com/${instagram}" target="blank"><img align="center" src="https://github.com/Prajith004/gh-profile-readme-generator/blob/main/images/instagram.svg"  height="30" width="40" /></a>
+<a href="https://www.youtube.com/c/${youtube}" target="blank"><img align="center" src="https://github.com/Prajith004/gh-profile-readme-generator/blob/main/images/youtube.svg"  height="30" width="40" /></a>
 </p>
 
 
